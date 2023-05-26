318074910b9f7687acd4d297be844ab8ac6cda11
vnhtpvn0505 <vnhtpvn0505@gmail.com>
2023-05-27 00:59:47 +0700

    updat code

--- src/components/Experience .tsx
+++ src/components/Experience .tsx
@@ -1,3 +1,6 @@
+import { KyberNetwork } from "iconsax-react";
+
+
 const Experience = () => {
     return(
         <div className="container pt-24">
@@ -6,7 +9,7 @@ const Experience = () => {
                 <h3 className="text-[40px] font-black text-green-500">My Resume</h3>
                  <div className="w-full pt-[30px] ">
                     {/* Education */}
-                     <div className="grid grid-cols-3 py-4 ">
+                     <div className="grid grid-cols-3 py-4 text-sm ">
                         <div className="p-4 bg-teal-500" >
                             <p className="font-bold  text-stone-200">Education</p>
                         </div>
@@ -27,61 +30,67 @@ const Experience = () => {
                         </div>
                      </div>
                     {/* Experience  */}
-                     <div className="grid grid-cols-3 py-4 ">
+                     <div className="grid grid-cols-3 py-4 text-sm">
                         <div className="p-4 md:p-2 bg-emerald-500	" >
                             <p className="font-bold">Experience</p>
                         </div>
                         <div className="text-left bg-cyan-100/[0.1] p-4 gap-y-8">
-                            <span className="font-semibold text-neutral-400">UX/UI Designer</span> <br/>
+                            <div className="flex">
+                            <KyberNetwork size="16" color="#059669" variant="Bold" /> <span className="font-semibold text-neutral-400"> UX/UI Designer</span> <br/>
+                            </div>
                             <span className=" text-neutral-600">Research & design UX, for product of company & customers.
                                     Design UI/UX  for apps blockchain, DApp &  Game-Fi , ect...
                             </span>
                         </div>
                         <div className="bg-cyan-100/[0.1] p-4">
-                            <p  className="font-semibold break-words">LuckyTech</p> <p>( Jan/2022 – Present)</p>
+                            <p  className="font-semibold break-words text-sm">LuckyTech</p> <p className="text-sm">( Jan/2022 – Present)</p>
                         </div>
                         <div className="bg-emerald-500">
                             
                         </div>
                         <div className="text-left p-4  bg-cyan-200/[0.1]">
-                            <span className="font-semibold text-neutral-400">UI/UX Designer</span>
+                        <div className="flex">
+                            <KyberNetwork size="16" color="#059669" variant="Bold" /> <span className="font-semibold text-neutral-400"> UX/UI Designer</span> <br/>
+                        </div>
                             <br/>
-                            <span className=" text-neutral-600">Design web layout & UX for customer of company with many different fields : 
+                            <span className="text-sm text-neutral-600">Design web layout & UX for customer of company with many different fields : 
                                         Real Estate, Education, Travel, Ecommerce, ect...
                             </span>
                         </div>
                         <div className="p-4  bg-cyan-100/[0.1]">
-                            <p  className="font-semibold">Time Universal</p> <p>(Jun 2021 – Nov 2021)</p>
+                            <p  className="font-semibold ">Time Universal</p> <p className="text-sm">(Jun 2021 – Nov 2021)</p>
                         </div>
                         <div className="bg-emerald-500">
                             
                         </div>
                         <div className="text-left p-4  bg-cyan-100/[0.1]">
-                            <span className="font-semibold text-neutral-400">UI Designer Intern</span>
+                        <div className="flex">
+                            <KyberNetwork size="16" color="#059669" variant="Bold" /> <span className="font-semibold text-neutral-400"> UI Designe Intern</span> <br/>
+                        </div>
                             <br/>
                             <span className=" text-neutral-600">Design web layout  Agency website for company ect...
                             </span>
                         </div>
                         <div className="p-4  bg-cyan-100/[0.1]">
-                            <p  className="font-semibold">Serdao </p> <p>(Nov 2020 - Apr 2021)</p>
+                            <p  className="font-semibold">Serdao </p> <p className="text-sm">(Nov 2020 - Apr 2021)</p>
                         </div>
                         <div className="bg-emerald-500">
                             
                         </div>
                         <div className="text-left p-4  bg-cyan-100/[0.1]">
-                            <span className="font-semibold text-neutral-400">Software Engineer</span>
-                            <br/>
-                            <span className=" text-neutral-600">Development for mobile  & web application  project as required of company & customer
-with Android, iOS, React-native, Flutter...
+                        <div className="flex">
+                            <KyberNetwork size="16" color="#059669" variant="Bold" /> <span className="font-semibold text-neutral-400">Software Engineer</span> <br/>
+                        </div>
+                            <span className=" text-neutral-600">Development for mobile  & web application  project as required of company & customer with Android, iOS, React-native, Flutter...
                             </span>
 
                         </div>
                         <div className="p-4  bg-teal-100/[0.1]">
-                            <p  className="font-semibold">Kyanon Digital</p> <p>(Dec 2016 – May 2020)</p>
+                            <p  className="font-semibold">Kyanon Digital</p> <p className="text-sm">(Dec 2016 – May 2020)</p>
                         </div>
                      </div>
                      {/* Certifications */}
-                     <div className="grid grid-cols-3 py-4 ">
+                     <div className="grid grid-cols-3 py-4 text-sm">
                         <div className="p-4 md:p-2 bg-cyan-500" >
                             <p className="font-bold">Certifications</p>
                         </div>
