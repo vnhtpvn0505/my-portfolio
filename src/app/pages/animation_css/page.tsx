b308b50b48f7abb84e233e356dfe34d881b65592
vnhtpvn0505 <vnhtpvn0505@gmail.com>
2023-06-13 01:16:50 +0700

    ipdate code

--- src/app/pages/animation_css/page.tsx
+++ src/app/pages/animation_css/page.tsx
@@ -6,9 +6,9 @@ export default function Animation() {
   return (
     <>
     <div>
-    <h1 className="title">
-                        Read <Link href="pages/animation_css/timing">this page!</Link>
-                </h1>
+      <h1 className="title">
+                          Read <Link href="pages/animation_css/transactions">this page!</Link>
+      </h1>
     </div>
     
   </>
---
+++ src/app/pages/animation_css/transactions/page.tsx
@@ -0,0 +1,20 @@
+
+import Link from 'next/link'
+import '../styles/animation.css'
+export default function Transaction() {
+
+    return (
+      <body>
+        <div className='container'>
+        <h1 className='text-4xl text-sky-500'>CSS Transactions</h1>
+        <p>Css Transactions allows you to change property values smoothly, over a given duration.</p>
+         <div>
+          <h1 className="title">
+              <Link href="pages/animation_css/transactions/timing">Transactions timing</Link>
+          </h1>
+         </div>
+      </div>
+      </body>
+
+    )
+  }
